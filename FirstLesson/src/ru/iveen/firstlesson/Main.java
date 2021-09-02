a4b847d66ad07ed9e9742826e446c9be0c16b38e
Anton Polyakov <antonpolyakovvv@mail.ru>
2021-09-02 16:22:21 +0300

    First task

---
+++ FirstLesson/src/ru/iveen/firstlesson/Main.java
@@ -0,0 +1,19 @@
+package ru.iveen.firstlesson;
+
+public class Main {
+    public static void main(String[] args){
+
+        // Первое задание
+        FindNum findnum = new FindNum();
+
+        int[] nums = new int[4];
+        nums[0] = 4;
+        nums[1] = 9;
+        nums[2] = 11;
+        nums[3] = 14;
+        System.out.println(findnum.findNum(nums, 10));
+
+        // Второе задание
+
+    }
+}
