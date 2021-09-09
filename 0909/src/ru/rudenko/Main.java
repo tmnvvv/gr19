cb9b20bfc91adfcb31b90c0e5adbe1fd26033b4c
Vadim <64652301+iroQQuois@users.noreply.github.com>
2021-09-09 16:02:59 +0300

    Delete 0909 directory

--- 0909/src/ru/rudenko/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.rudenko;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println(PowerOfTwo.checkPowerOfTwo(4));
-        LogicOperations.doOperations(5, 10, 15);
-    }
-}
