4678e65c1c97525fed47f103941bb36b399474fa
Vadim <64652301+iroQQuois@users.noreply.github.com>
2021-09-09 15:59:24 +0300

    Add files via upload

---
+++ 0909/src/ru/rudenko/Main.java
@@ -0,0 +1,9 @@
+package ru.rudenko;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(PowerOfTwo.checkPowerOfTwo(4));
+        LogicOperations.doOperations(5, 10, 15);
+    }
+}
